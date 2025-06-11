4d584fd48b61fef8a93b135209004f3767b6289b
HamzaFiaz <hamzafiaz708@gmail.com>
2025-06-11 23:37:51 +0500

    add files

---
+++ lib/adaptive_ui/platform_adaptive.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class PlatformAdaptive extends StatefulWidget {
+  const PlatformAdaptive({super.key});
+
+  @override
+  State<PlatformAdaptive> createState() => _PlatformAdaptiveState();
+}
+
+class _PlatformAdaptiveState extends State<PlatformAdaptive> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Platform Adaptive"),
+      ),
+         body: Center(
+           child:  Platform.isIOS ? SizedBox(
+             child: CupertinoButton(
+               onPressed: (){},
+                 child: const Text("Cupertino Button"),
+             ),
+           ): SizedBox(
+             child: ElevatedButton(
+                 onPressed: (){},
+                 child: const Text("Elevated Button")
+             ),
+           ),
+         ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lottie_proj/animated_list/animated_list.dart';
-import 'package:lottie_proj/clippath_widget/clip_path.dart';
-import 'package:lottie_proj/clippath_widget/triangle_clipper.dart';
-import 'package:lottie_proj/hero_animation/hero_animation.dart';
-import 'package:lottie_proj/implicit_and_explicit_animation/i.dart';
-import 'package:lottie_proj/physics_based_animation/physics_based_animation.dart';
-import 'package:lottie_proj/staggered_animation/staggered_animation.dart';
-import 'package:lottie_proj/ui/s.dart';
 import 'package:lottie_proj/ui/shimar_load_list.dart';
+import 'package:sizer/sizer.dart';
 
 
 
@@ -22,14 +14,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: Size(390, 800),
-      minTextAdapt: true,
-      splitScreenMode: true ,
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: S(),
-      ),
+    // return const ScreenUtilInit(
+    //   designSize: Size(390, 800),
+    //   minTextAdapt: true,
+    //   splitScreenMode: true ,
+    //   child: MaterialApp(
+    //     debugShowCheckedModeBanner: false,
+    //     home: ScreenUtilGrid(),
+    //   ),
+    // );
+    return Sizer(
+      builder: (context,orientation,deviceType){
+        return const MaterialApp(
+          home: ShimarListScreen(),
+        );
+      }
+
     );
   }
 }
